5027a35207562668a7194952f37b14c8fd2efc52
JSimko4 <xsimkoj2@stuba.sk>
2022-03-19 17:00:32 +0100

    added all routes & view for rest api

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views
-from .views import ExerciseView
+from .views import GetBodyPartsView, GetFilterExercisesView, GetExerciseView, GetAllExercisesView, \
+    SaveExerciseView, EditExerciseView, DeleteExerciseView
 
 urlpatterns = [
-    path("<int:user_id>/", views.user_exercises),
-    path("<int:user_id>/save_exercise/", ExerciseView.as_view())
+    path("<int:user_id>/body_parts", GetBodyPartsView.as_view()),
+    path("<int:user_id>/body_parts", GetFilterExercisesView.as_view()),
+    path("<int:user_id>/", GetExerciseView.as_view()),
+    path("<int:user_id>/body_parts", GetAllExercisesView.as_view()),
+    path("<int:user_id>/save_exercise/", SaveExerciseView.as_view()),
+    path("<int:user_id>/edit/", EditExerciseView.as_view()),
+    path("<int:user_id>/delete/", DeleteExerciseView.as_view())
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -8,27 +8,54 @@ from .serializers import ExerciseSerializer
 from .models import Exercise
 
 
-def user_exercises(request, user_id: int):
-    try:
-        cursor = connection.cursor()
-        cursor.execute("SELECT VERSION();")
-        version = cursor.fetchone()[0]
+# def user_exercises(request, user_id: int):
+#     try:
+#         cursor = connection.cursor()
+#         cursor.execute("SELECT VERSION();")
+#         version = cursor.fetchone()[0]
+#
+#         cursor.close()
+#         connection.close()
+#
+#         return JsonResponse({
+#             "mysql": {
+#                 "test mysql version": version,
+#                 "user_id": user_id
+#             }
+#         })
+#
+#     except Exception as error:
+#         return HttpResponse(f"Error while connecting to PostgreSQL {error}")
 
-        cursor.close()
-        connection.close()
 
-        return JsonResponse({
-            "mysql": {
-                "test mysql version": version,
-                "user_id": user_id
-            }
-        })
-
-    except Exception as error:
-        return HttpResponse(f"Error while connecting to PostgreSQL {error}")
+class GetBodyPartsView(APIView):
+    def get(self, request):
+        print("x")
 
 
-class ExerciseView(APIView):
+class GetFilterExercisesView(APIView):
+    def get(self, request):
+        print("x")
+
+
+class GetExerciseView(APIView):
+    def get(self, request, id=None):
+        if id:
+            item = Exercise.objects.get(id=id)
+            serializer = ExerciseSerializer(item)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+        items = Exercise.objects.all()
+        serializer = ExerciseSerializer(items, many=True)
+        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+
+
+class GetAllExercisesView(APIView):
+    def get(self, request):
+        print("x")
+
+
+class SaveExerciseView(APIView):
     def post(self, request, user_id: int):
         request.data["user_id"] = user_id
 
@@ -38,3 +65,13 @@ class ExerciseView(APIView):
             return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class EditExerciseView(APIView):
+    def put(self, request):
+        print("x")
+
+
+class DeleteExerciseView(APIView):
+    def delete(self, request):
+        print("x")

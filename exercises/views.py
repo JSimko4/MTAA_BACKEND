a9ada7dc5baac0c7b41f685376e7991d0ff324cc
JSimko4 <xsimkoj2@stuba.sk>
2022-03-19 20:00:22 +0100

    keyerror fix

--- exercises/views.py
+++ exercises/views.py
@@ -38,14 +38,17 @@ class SaveExerciseView(APIView):
         if serializer.is_valid():
             # pridat osetrenie ked neexistuje dany user -> zla url id
             new_exercise = Exercise.objects.create(user=User.objects.get(id=user_id), name=request.data["name"],
-                                                   description=request.data["description"], image_path=request.data["image_path"])
+                                                   description=request.data["description"],
+                                                   image_path=request.data["image_path"])
             new_exercise.save()
 
-            # pridat osetrenie ak neexistuje dany body part
-            body_parts = request.data["body_parts"]
-            for body_part_id in body_parts:
-                body_part = BodyPart.objects.get(id=body_part_id)
-                new_exercise.body_parts.add(body_part)
+            if "body_parts" in request.data:
+                body_parts = request.data["body_parts"]
+
+                # pridat osetrenie ak neexistuje dany body part
+                for body_part_id in body_parts:
+                    body_part = BodyPart.objects.get(id=body_part_id)
+                    new_exercise.body_parts.add(body_part)
 
             return Response({"status": "success"}, status=status.HTTP_200_OK)
         else:
